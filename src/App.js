8878e52188c93ff83536f02503ad647d4952aa9b
Gabor <gabor.vlad13@gmail.com>
2020-11-06 13:19:19 +0200

    hard coded user item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-
+import Users from './components/users/Users';
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
---
+++ src/components/users/Users.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: 'id',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo',
+      },
+      {
+        id: 'id1',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo',
+      },
+      {
+        id: 'id2',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo',
+      },
+    ],
+  };
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map(user => (
+          <UserItem user={user} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem  ',
+};
+export default Users;
